aa8761ae2486372a7f6f3b17bacb9b2b1ff7ec25
Максим Давыдов <maxwest2002@outlook.com>
2024-09-28 18:46:44 +0700

    Добавьте файлы проекта.

---
+++ OpenWeatherAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+
+namespace RandomAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly HttpClient _client;
+        private readonly IDatabase _redis;
+
+        public WeatherForecastController(HttpClient client, IConnectionMultiplexer muxer)
+        {
+            _client = client;
+            _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weatherCachingApp", "1.0"));
+            _redis = muxer.GetDatabase();
+        }
+
+        private async Task<string> GetForecast(double latitude, double longitude)
+        {
+            var pointsRequestQuery = $"https://api.weather.gov/points/{latitude.ToString(System.Globalization.CultureInfo.InvariantCulture)},{longitude.ToString(System.Globalization.CultureInfo.InvariantCulture)}"; //get the URI
+            var result = await _client.GetFromJsonAsync<JsonObject>(pointsRequestQuery);
+            var gridX = result["properties"]["gridX"].ToString();
+            var gridY = result["properties"]["gridY"].ToString();
+            var gridId = result["Properties"]["gridId"].ToString();
+            var forecastRequestQuery = $"https://api.weather.gov/gridpoints/{gridId}/{gridX},{gridY}/forecast";
+            var forecastResult = await _client.GetFromJsonAsync<JsonObject>(forecastRequestQuery);
+            var periodsJson = forecastResult["properties"]["periods"].ToJsonString();
+            return periodsJson;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<ForecastResult> Get([FromQuery] double latitude, [FromQuery] double longitude)
+        {
+            string json;
+            var watch = Stopwatch.StartNew();
+            var keyName = $"forecast:{latitude},{longitude}";
+            json = await _redis.StringGetAsync(keyName);
+            if (string.IsNullOrEmpty(json))
+            {
+                json = await GetForecast(latitude, longitude);
+                var setTask = _redis.StringSetAsync(keyName, json);
+                var expireTask = _redis.KeyExpireAsync(keyName, TimeSpan.FromSeconds(3600));
+                await Task.WhenAll(setTask, expireTask);
+            }
+
+            var forecast = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(json);
+            watch.Stop();
+            var result = new ForecastResult(forecast, watch.ElapsedMilliseconds);
+
+            return result;
+        }
+    }
+
+}
---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,25 @@
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost"));
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
